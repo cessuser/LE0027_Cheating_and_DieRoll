ac771028e31651f120a948ab6b75016164de7322
Danlin Chen <d84chen@uwaterloo.com>
2018-11-28 13:46:37 +0800

    total real

--- LastModel_RET/pages.py
+++ LastModel_RET/pages.py
@@ -22,7 +22,8 @@ class Results(Page):
             'pay3': self.player.participant.vars['m3_payoff'],
             'pay4': self.player.participant.vars['M4_payoff_real_currency'],
             'pay5': self.player.participant.vars['M5_payoff'],
-            'total': self.player.final_ECUs
+            'total': self.player.final_ECUs,
+            'total_real': self.player.final_ECUs * self.session.config['real_world_currency_per_point']
         }
 
 class Survey(Page):
